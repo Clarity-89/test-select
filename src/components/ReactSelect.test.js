b231138bd3a19e259a3b5cc98ff4ad6e7f6ba1f2
Clarity-89 <homes89@ukr.net>
2023-05-11 13:17:07 +0300

    Use selectOptions

--- src/components/ReactSelect.test.js
+++ src/components/ReactSelect.test.js
@@ -1,4 +1,4 @@
-import { act, render, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { select } from "react-select-event";
 import userEvent from "@testing-library/user-event";
 
@@ -15,60 +15,43 @@ describe("ReactSelectForm", () => {
   it("should render with default value selected", () => {
     setup(<ReactSelectForm defaultValue={{ value: "cat", label: "Cat" }} />);
     expect(screen.getByText("Cat")).toBeInTheDocument();
-    expect(screen.queryByText("Dog")).not.toBeInTheDocument();
-    expect(screen.getByLabelText("animal form")).toHaveFormValues({
-      animals: "cat",
-    });
+    expect(screen.getByRole("form")).toHaveFormValues({ animals: "cat" });
   });
 
   it("should select correct value on change", async () => {
     const { user } = setup(
-      <ReactSelectForm
-        defaultValue={{ value: "cat", label: "Cat" }}
-        inputId={"animals"}
-      />
+      <ReactSelectForm defaultValue={{ value: "cat", label: "Cat" }} />
     );
 
-    await act(async () => {
-      await user.click(screen.getByLabelText("Animals"));
-      // screen.getByLabelText("Animals").focus();
-      // await user.keyboard("[ArrowDown]");
-    });
-    await act(async () => {
+    await waitFor(async () => {
+      // await user.click(screen.getByLabelText("Animals"));
+      screen.getByLabelText("Animals").focus();
+      await user.keyboard("[ArrowDown]");
       await user.click(screen.getByText("Zebra"));
     });
 
-    expect(screen.getByLabelText("animal form")).toHaveFormValues({
-      animals: "zebra",
-    });
+    expect(screen.getByRole("form")).toHaveFormValues({ animals: "zebra" });
   });
 
   it("should work with select-event", async () => {
-    const mockUpdate = jest.fn();
-    setup(
-      <ReactSelectForm
-        defaultValue={{ value: "cat", label: "Cat" }}
-        inputId={"animals"}
-        onChange={mockUpdate}
-      />
-    );
+    setup(<ReactSelectForm defaultValue={{ value: "cat", label: "Cat" }} />);
 
-    await waitFor(() => select(screen.getByLabelText("Animals"), "Zebra"));
+    await selectOptions(screen.getByLabelText("Animals"), "Zebra");
 
-    expect(screen.getByLabelText("animal form")).toHaveFormValues({
-      animals: "zebra",
-    });
+    expect(screen.getByRole("form")).toHaveFormValues({ animals: "zebra" });
   });
 
   it("should work with multi-select", async () => {
-    setup(<ReactSelectForm inputId={"animals"} isMulti />);
+    setup(<ReactSelectForm isMulti />);
 
-    await waitFor(() =>
-      select(screen.getByLabelText("Animals"), ["Zebra", "Lion"])
-    );
+    await selectOptions(screen.getByLabelText("Animals"), ["Zebra", "Lion"]);
 
-    expect(screen.getByLabelText("animal form")).toHaveFormValues({
+    expect(screen.getByRole("form")).toHaveFormValues({
       animals: ["zebra", "lion"],
     });
   });
 });
+
+const selectOptions = async (input, options) => {
+  await waitFor(() => select(input, options));
+};
